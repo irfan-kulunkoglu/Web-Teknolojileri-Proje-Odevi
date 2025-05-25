7d8a63ace28223f56cd9e7016b4c4f9a0bfbf4b2
irfan-kulunkoglu <irfan.kulunkoglu@ogr.sakarya.edu.tr>
2025-05-25 17:50:42 +0300

    ilgi-alanlarim.html dosyasını güncelledim.
    Dosyanın kendisini güncelledim. Dosya ile bağlantılı gerekli php dosyalarını yükledim.

---
+++ getdata_pd.php
@@ -0,0 +1,14 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+$api_url = "https://api.football-data.org/v4/competitions/PD/standings";
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $api_url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, [
+    "X-Auth-Token: 214319d70a544ce9bad2ce887eb1dbbd"
+]);
+$response = curl_exec($ch);
+curl_close($ch);
+header('Content-Type: application/json');
+echo $response;
+?>
